a51ac212a9d66edb1664f1d7b3cb9f75ffced0a1
Adrien Zier <adrie.zier@gmail.com>
2020-08-22 21:06:57 +0200

    added scanner component

--- Maskit/Controllers/HomeViewController.swift
+++ Maskit/Controllers/HomeViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HomeViewController: UIViewController {
 
     var VIEW_HEIGHT = CGFloat()
     var margins = UILayoutGuide()
+    
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
     
     fileprivate let headerTitle: UILabel = {
             
@@ -240,6 +244,50 @@ class HomeViewController: UIViewController {
         initialise()
         generateUI()
         
+        DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
+            
+            guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+                print("your device is not applicable for video recording")
+                return
+            }
+            
+            let avVideoInput: AVCaptureDeviceInput
+            
+            do {
+                avVideoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+            } catch {
+                print("your device has no camera")
+                return
+            }
+            
+            if (self.captureSession.canAddInput(avVideoInput)) {
+                
+                self.captureSession.addInput(avVideoInput)
+                
+            } else {
+                print("your device cannot add input in capture session")
+                return
+                
+            }
+            
+            let metadataOutput = AVCaptureMetadataOutput()
+            
+            if (self.captureSession.canAddOutput(metadataOutput)) {
+                self.captureSession.addOutput(metadataOutput)
+                metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+                metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417, .qr]
+            } else {
+                return
+            }
+            
+            self.previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+            self.previewLayer.frame = self.view.layer.bounds
+            self.previewLayer.videoGravity = .resizeAspectFill
+            self.view.layer.addSublayer(self.previewLayer)
+            self.captureSession.startRunning()
+            
+        })
+        
     }
     
     func initialise() {
@@ -366,6 +414,26 @@ class HomeViewController: UIViewController {
         guard let url = URL(string: "https://www.rdehospital.nhs.uk/documents/patients/wearing-a-face-mask-or-face-covering-during-covid-19-leaflet.pdf") else { return }
         UIApplication.shared.open(url)
         
+    }
+}
+
+extension HomeViewController : AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if let first = metadataObjects.first{
+            guard let readableObject = first as? AVMetadataMachineReadableCodeObject else {
+                return
+            }
+            guard let stringValue = readableObject.stringValue else {
+                return
+            }
+            found(code: stringValue)
+        }
+        else {
+            print("Not able to read the code! Please try again.")
+        }
+    }
+    func found(code: String){
+        print(code)
     }
 }
 
